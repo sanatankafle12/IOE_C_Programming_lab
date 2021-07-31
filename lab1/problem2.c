ca445d5e9fed5eac521fe912d0ce2c703dab8a88
Sanatan Kafle <kaflesanatan100@gmail.com>
2021-07-31 22:18:46 +0545

    Sum of two numbers

---
+++ lab1/problem2.c
@@ -0,0 +1,13 @@
+/*Write a program to add two numbers (5&7) and display its sum.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1=5;
+    int num2=7;
+    int sum;
+    sum = num1+num2;
+    printf("The sum is %d",sum);
+    return 0;
+}
