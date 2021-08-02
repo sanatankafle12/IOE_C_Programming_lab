9c154459ec022afbff44cfbadc2591d2fb5aea79
Sanatan Kafle <kaflesanatan100@gmail.com>
2021-08-02 23:20:36 +0545

    Added the second problem

--- lab1/problem2.c
+++ lab1/problem2.c
@@ -1,13 +1,13 @@
-/*Write a program to add two numbers (5&7) and display its sum.*/
+/*write a program to calculate  the volume of sphere of radius r*/
 
 #include <stdio.h>
 
 int main()
 {
-    int num1=5;
-    int num2=7;
-    int sum;
-    sum = num1+num2;
-    printf("The sum is %d",sum);
+    float r,vol,pi=3.14;
+    printf("Enter the radius: ");
+    scanf("%f",&r);
+    vol = 4.0/3.0*pi*r*r*r;
+    printf("The volume of the sphere with radius %f is %.2f.",r,vol);
     return 0;
 }
