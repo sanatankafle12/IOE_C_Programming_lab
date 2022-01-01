efc582dca0334ecd2025c7c7599650c649f79b61
Sanatan Kafle <kaflesanatan100@gmail.com>
2022-01-01 21:28:18 +0545

    added lab2

---
+++ lab2/problem5.c
@@ -0,0 +1,9 @@
+/* . Write a prograrite programs to evaluate the following expression:
+a. S=x^5 +0.2xy+y^7
+b. f = (a+b)^((2x+y)/(p-q)) + c-100
+c. r = A/B
+d. r = (u/x+v/y)
+*/
+
+#include <stdio.h>
+
